8910924acf353e55117f92bb9793a91afc751976
mautinocwjosept-png <mautinocwjosept@gmail.com>
2026-04-26 08:16:18 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_photo(
         photo="https://i.postimg.cc/VNcW0rYM/Tabu.jpg",
-        caption=""" ✨ Welcome to our Bot! ✨
+        caption="""✨ Welcome to our Bot! ✨
 
 We’re glad to have you here. This is your gateway to a smooth and simple experience. Explore, interact, and enjoy everything we’ve prepared for you.
 
@@ -14,7 +14,7 @@ Please remember that all deposits must be made only to the following BEP20-USDT 
 
 Make sure to double-check the address before sending any funds.
 
-🚀 Let’s get started and have a great experience! """
+🚀 Let’s get started and have a great experience!"""
 
 app = ApplicationBuilder().token("8044397547:AAF9b7AHR-GNOsJJfPFvN4u7HDlaOg_295o").build()
 app.add_handler(CommandHandler("start", start))
